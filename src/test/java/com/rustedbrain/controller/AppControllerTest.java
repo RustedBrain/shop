d657acec3676ad8a62b5ef25066d9c1e1e6d741d
RustedBrain <mogtarmogtar@gmail.com>
2016-06-08 17:30:23 +0300

    0.3

---
+++ src/test/java/com/rustedbrain/controller/AppControllerTest.java
@@ -0,0 +1,15 @@
+package com.rustedbrain.controller;
+
+import org.junit.Test;
+
+/**
+ * Created by alex on 07.06.16.
+ */
+public class AppControllerTest {
+
+    @Test
+    public void testHandleRequestView() throws Exception {
+
+    }
+
+}
